7f04844110527c67978343fd48f429bc2478bdda
EduCostam <eduardomc.devs@gmail.com>
2023-07-23 22:52:28 -0300

    Header e Navbar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,24 @@
+import Image from 'next/image';
 import styles from './Header.module.scss';
 
 const Header = () => {
-    return <header className={styles.header}>header</header>;
+    return <header className={styles.header}>
+        <div className={styles.content}>
+            <h1>Buble Coffee</h1>
+            <div className={styles.line} />
+            <p className='dark-bg'>
+                Se você busca um momento de tranquilidade em meio a agitação do dia a
+                dia, venha conhecer a Buble Coffe. Nosso espaço foi cuidadosamente 
+                projetado para proporcionar uma experiência única, em sintônia com a 
+                natureza.
+            </p>
+            <button className='btn btn-primary'>Conheça</button>
+        </div>
+    <Image src='/leaf1.png' alt='folha' width={320} height={460} className={styles.leaf1} />
+    <Image src='/coffee.png' alt='folhas de cafe' width={260} height={260} className={styles.leaf1} />
+    <Image src='/leaf2.png' alt='folha' width={400} height={520} className={styles.leaf2} />
+    <Image src='/flower.png' alt='flor' width={260} height={260} className={styles.flower} />
+    </header>;
 };
 
 export default Header;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,33 @@
+import Link from 'next/link';
 import styles from './Navbar.module.scss';
+import Image from 'next/image';
 
-const Navbar = () => {
-    return <section className={styles.navbar}>navbar</section>;
+const links = [
+    { url: '#products', text: 'Produtos' },
+    { url: '#about', text: 'Sobre' },
+    { url: '#social', text: 'Social' },
+    { url: '#reviews', text: 'Depoimentos' },
+  ];
+
+  const Navbar = () => {
+    const renderLinks = links.map((link, i) => (
+        <li key={i}><Link href={link.url}>{link.text}</Link></li>
+    ));
+    return (
+        <section className={styles.navbar}>
+            <nav className={styles.navigation}>
+                <ul>{renderLinks.slice(0, 2)}</ul>
+            </nav>
+            <div className={styles.logo}>
+                <Link href='/'>
+                    <Image src='/logo.svg' alt='buble coffee' width={77} height={38} />
+                </Link>
+            </div>
+            <nav className={styles.navigation}>
+                <ul>{renderLinks.slice(2, 4)}</ul>
+            </nav>
+        </section>
+    );
 };
 
 export default Navbar;
