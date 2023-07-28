c8aad79de311dddea98e6ded2850ebda37c7a5b0
EduCostam <eduardomc.devs@gmail.com>
2023-07-27 22:54:12 -0300

    Products e Product

--- src/Components/Products/Product.jsx
+++ src/Components/Products/Product.jsx
@@ -1,11 +1,20 @@
+
 import Image from 'next/image';
 import styles from './Product.module.scss';
+import Stars from '@/UI/Stars';
 
-const Product = () => {
+const Product = ({ product, title, roast, stars, reviews, price}) => {
     return (
         <div className={styles.product}>
-            <Image src='/product1png' alt='produto 1' width={200} height={300} />
-            <h2></h2>
+            <Image src={`/product${product}.png`}
+            alt={`product${product}`}
+            width={200}
+            height={300}
+            />
+            <h2>{title}</h2>
+            <h3>{roast}</h3>
+            <Stars stars={stars} reviwes={reviews}/>
+            <h4>R$ {price}</h4>
         </div>
     )
 }
--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,7 +1,13 @@
+import Product from './Product';
 import styles from './Products.module.scss';
 
 const Products = () => {
-    return <section className={styles.products}>products</section>;
+    return <section className={styles.products}>
+        <Product product='1' title='Café Blend' roast='Torra Escura' stars='5' reviews='16' price='29,90'/>
+        <Product product='2' title='Café Branco' roast='Torra Clara' stars='5' reviews='12' price='28,50'/>
+        <Product product='3' title='Café Premium' roast='Torra Média' stars='5' reviews='8' price='32,90'/>
+        <Product product='4' title='Café Latte' roast='Torra Clara' stars='5' reviews='7' price='25,00'/>
+    </section>;
 };
 
 export default Products;
